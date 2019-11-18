ca3b2780cdf74d1ad00627b981453a14b3df8d61
Joshua Lipps <joshslipps@gmail.com>
2019-11-18 10:47:33 -0500

    front end update calendar doesnt work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
+import { CalendarModule, DateAdapter } from "angular-calendar";
+import { adapterFactory } from "angular-calendar/date-adapters/date-fns";
 import { ProfileComponent } from "./components/profile/profile.component";
 
 @NgModule({
@@ -14,7 +16,14 @@ import { ProfileComponent } from "./components/profile/profile.component";
     RegisterComponent,
     ProfileComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
