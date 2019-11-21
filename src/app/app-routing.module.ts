696c1d54a53d7e318d0711dfd28282f3445fac75
Joshua Lipps <joshslipps@gmail.com>
2019-11-21 11:10:51 -0500

    basic angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { CalendarComponent } from "./components/calendar/calendar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "calendar",
+    component: CalendarComponent
+  },
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,21 @@ import { CalendarModule, DateAdapter } from "angular-calendar";
 import { adapterFactory } from "angular-calendar/date-adapters/date-fns";
 import { ProfileComponent } from "./components/profile/profile.component";
 import { CalendarComponent } from "./components/calendar/calendar.component";
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCtokLWJBEb8N1veSWVZkI4cO1Dze3jh44",
+  authDomain: "calendar-app-7239b.firebaseapp.com",
+  databaseURL: "https://calendar-app-7239b.firebaseio.com",
+  projectId: "calendar-app-7239b",
+  storageBucket: "calendar-app-7239b.appspot.com",
+  messagingSenderId: "265895014653",
+  appId: "1:265895014653:web:a29b65749dcc0559dece94",
+  measurementId: "G-X7N3QV3F76"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +38,9 @@ import { CalendarComponent } from "./components/calendar/calendar.component";
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
-    })
+    }),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
